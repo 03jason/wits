ba9bdd5a8327c39397c01d00bd25da5a13c54f65
jason <jasri1998@gmail.com>
2025-10-06 15:55:09 +0200

    chore: arborescence clean (réorganisation des dossiers)

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 // garde UN seul import CSS qui existe réellement
-import './styles.css'; // supprime l'autre import s'il n'existe pas
+import './siteWits/assets/styles.css'; // supprime l'autre import s'il n'existe pas
 
 console.log('[WITS] PRODUCTS_API =', import.meta.env.VITE_PRODUCTS_API_URL);
 console.log('[WITS] MOVEMENTS_API =', import.meta.env.VITE_MOVEMENTS_API_URL);
